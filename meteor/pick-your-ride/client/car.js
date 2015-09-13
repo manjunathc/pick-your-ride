01531af807df161df45d55a4f4d9baa0c53530d6
sagarhazary <sagarhazary@gmail.com>
2015-09-13 11:35:43 -0700

    CAR

---
+++ meteor/pick-your-ride/client/car.js
@@ -0,0 +1,42 @@
+if (Meteor.isClient) {
+
+Template.body.helpers({
+  cars: function()
+  {
+    return Cars.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.body.events({
+
+
+  "submit .new-car": function(event)
+  {
+    // Prevent default browser form submit
+      event.preventDefault();
+
+      //Get Value from the Car Body Form
+    var vin= event.target.vin.value;
+    var make= event.target.make.value;
+    var model= event.target.model.value;
+    var year= event.target.year.value;
+    var trim= event.target.trim.value;
+
+//Insert Data in to Cars Mongo Collection
+    Cars.insetr({
+      vin: vin,
+      make: make,
+      model: model,
+      year: year,
+      createdAt: new Date()
+    });
+    // Clear form
+      event.target.vin.value = "";
+        event.target.make.value = "";
+          event.target.model.value = "";
+            event.target.year.value = "";
+
+  }
+}
+)
+}
---
+++ meteor/pick-your-ride/collections/cars.js
@@ -0,0 +1,1 @@
+Cars = new Mongo.Collection('cars');
